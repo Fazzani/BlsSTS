5796b57eb91b0ea4669e1537b535d85d4159af68
Fazzani <tunisienheni@outlook.com>
2015-10-05 23:33:58 +0200

    HFA: Test SSO

---
+++ WebTestClient1/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebTestClient1
+{
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
+    {
+      AreaRegistration.RegisterAllAreas();
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+      BeginRequest += Application_BeginRequest;
+    }
+
+    private void Application_BeginRequest(object sender, EventArgs e)
+    {
+      if (String.Compare(Request.Path, Request.ApplicationPath, StringComparison.InvariantCultureIgnoreCase) == 0
+          && !(Request.Path.EndsWith("/")))
+        Response.Redirect(Request.Path + "/");
+    }
+  }
+}
